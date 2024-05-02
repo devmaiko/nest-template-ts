8b089578805d9b70ee66d5351436b9edc5b4b44e
devmaiko <guivini8@gmail.com>
2024-05-02 20:43:58 -0300

    add apm

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,16 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as apm from 'elastic-apm-node';
 
 async function bootstrap() {
+  apm.start({
+    serviceName: 'nest-template',
+    secretToken: process.env.APM_SECRET_TOKEN,
+    serverUrl: process.env.APM_SERVER_URL,
+    environment: 'dev',
+  });
+
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT;
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
